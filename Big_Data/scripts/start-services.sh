e439205d1fdc3e6b897861cf2e77f775aac50597
Leonard Greene <925606751@qq.com>
2025-11-04 09:59:44 +0800

    temp

--- Big_Data/scripts/start-services.sh
+++ Big_Data/scripts/start-services.sh
@@ -11,5 +11,5 @@ nginx -t
 
 echo "启动Nginx（过滤调试信息）..."
 # 启动Nginx并过滤掉调试信息
-exec nginx -g "daemon off; error_log /dev/stderr error;" 2>&1 | \
-    grep -E '(\\[error\\]|[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}.*HTTP/[0-9]\\.[0-9]" [4-5][0-9][0-9])'
+# exec nginx -g "daemon off; error_log /dev/stderr error;" 2>&1 | \
+#     grep -E '(\\[error\\]|[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}.*HTTP/[0-9]\\.[0-9]" [4-5][0-9][0-9])'
