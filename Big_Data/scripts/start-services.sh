1997d15c3b965dc3cb8ea7e659c073754933d3a8
Leonard Greene <925606751@qq.com>
2025-11-04 10:01:36 +0800

    temp

--- Big_Data/scripts/start-services.sh
+++ Big_Data/scripts/start-services.sh
@@ -6,10 +6,10 @@ echo "🚀 开始启动HTTPS网站服务..."
 echo "删除默认的Nginx配置..."
 rm -f /etc/nginx/sites-enabled/default
 
-echo "测试Nginx配置..."
-nginx -t
+# echo "测试Nginx配置..."
+# nginx -t
 
-echo "启动Nginx（过滤调试信息）..."
+# echo "启动Nginx（过滤调试信息）..."
 # 启动Nginx并过滤掉调试信息
 # exec nginx -g "daemon off; error_log /dev/stderr error;" 2>&1 | \
 #     grep -E '(\\[error\\]|[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}.*HTTP/[0-9]\\.[0-9]" [4-5][0-9][0-9])'
